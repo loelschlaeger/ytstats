0f00064c9e2c034170f0cf4cfd2bad3a5e4e9864
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2022-09-22 16:41:04 +0200

    Initialized sandbox.

---
+++ sandbox.R
@@ -0,0 +1,14 @@
+# Read and save YouTube data ----------------------------------------------
+
+data <- read_yt(
+  email = "oelschlaeger.lennart@gmail.com",
+  from = "2020-01-01",
+  to = "2022-09-01"
+)
+
+saveRDS(data, "data/data.rds", compress = "xz")
+
+# Load YouTube data -------------------------------------------------------
+
+data <- readRDS("data/data.rds")
+
